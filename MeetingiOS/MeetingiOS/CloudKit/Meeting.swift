82124fe88b94be353727bd3873e1f425c8b39b64
Lucas <lucashpc91@gmail.com>
2019-11-25 19:49:50 -0300

    Struct Meeting Modificada
    Atributos e Construtor alterado.

--- MeetingiOS/MeetingiOS/CloudKit/Meeting.swift
+++ MeetingiOS/MeetingiOS/CloudKit/Meeting.swift
@@ -10,33 +10,99 @@ import Foundation
 import CloudKit
 
 /**
-Reunião 
+Struct utilizada para criar um nova reunião ou 
+utilizar como auxílio de manipulação de CKRecord
 - Author: Lucas Costa 
  */
 
 struct Meeting {
     
     //MARK:- Properties
-    var manager : CKRecord.Reference?
-    var duration : Int64?
-    var employees : [CKRecord.Reference] = []
-    var limitTopic : Int64?
-    var topic : [CKRecord.Reference] = []
-    var finished : Bool
-    var started : Bool
-    var theme : String
-    var date : Date?
+    
+    ///Record gerenciador dos atributos
+    var record : CKRecord?
+    
+    ///Gerenciador da reunião
+    var manager : CKRecord.Reference? {
+        didSet {
+            self.record?.setValue(manager, forKey: "manager")
+        }
+    }
+    
+    ///Duração da reunião
+    var duration : Int64? {
+        didSet {
+            self.record?.setValue(duration, forKey: "duration")
+        }
+    }
+    
+    ///Funcionários que participaram da reunião
+    var employees : [CKRecord.Reference] {
+        didSet {
+            self.record?.setValue(employees, forKey: "employees")
+        }
+    }
+    
+    ///Limite de tópicos de para cada funcionário
+    var limitTopic : Int64? {
+        didSet {
+            self.record?.setValue(limitTopic, forKey: "limitTopic")
+        }
+    }
+    
+    ///Tópicos da reunião
+    var topics : [CKRecord.Reference] {
+        didSet {
+            self.record?.setValue(topics, forKey: "topics")
+        }
+    }
+    
+    ///Reunião finalizada
+    var finished : Bool {
+        didSet {
+            self.record?.setValue(finished, forKey: "finished")
+        }
+    }
+    
+    ///Reunião iniciada
+    var started : Bool {
+        didSet {
+            self.record?.setValue(started, forKey: "started")
+        }
+    }
+    
+    ///Tema da reunião
+    var theme : String {
+        didSet {
+            self.record?.setValue(theme, forKey: "theme")
+        }
+    }
+    
+    ///Data da realização da reunião
+    var date : Date? {
+        didSet {
+            self.record?.setValue(date, forKey: "date")
+        }
+    }
     
     //MARK: - Initializer
     init(record : CKRecord) {
+        
+        if record.recordType != CKRecord.RecordType("Meeting") {
+            fatalError("Record diferente do tipo Meeting")
+        }
+            
+        self.record = record
         self.manager = record.value(forKey: "manager") as? CKRecord.Reference
-        self.duration = record.value(forKey: "duration") as? Int64 ?? 40
+        self.duration = record.value(forKey: "duration") as? Int64
         self.employees = record.value(forKey: "employees") as? [CKRecord.Reference] ?? []
         self.limitTopic = record.value(forKey: "limitTopic") as? Int64 ?? 3
         self.finished = record.value(forKey: "finished") as? Bool ?? false
         self.started = record.value(forKey: "started") as? Bool ?? false
         self.date = record.value(forKey: "date") as? Date
         self.theme = record.value(forKey: "theme") as? String ?? ""
+        self.topics = record.value(forKey: "topics") as? [CKRecord.Reference] ?? []
+        
     }
         
     //MARK:- Methods
@@ -48,6 +114,17 @@ struct Meeting {
     */
     mutating func addingNewEmployee(_ employee : CKRecord.Reference) {
         self.employees.append(employee)
+        self.record?.setValue(employees, forKey: "employees")
+    }
+    
+    /**
+     Remover funcionários da reunião
+    - parameters: 
+        - index : Indice do funcionário
+    */
+    mutating func removingEmployee(index : Int) {
+        self.employees.remove(at: index)
+        self.record?.setValue(employees, forKey: "employees")
     }
     
     /**
@@ -56,16 +133,8 @@ struct Meeting {
         - topic : Novo tópico
     */
     mutating func addingNewTopic(_ topic : CKRecord.Reference) {
-        self.employees.append(topic)
-    }
-    
-    /**
-     Modificando o limite de pautas
-        - parameters: 
-            - count : Quantidade de tópicos
-     */
-    mutating func changeNumbersOfTopic(count : Int64) {
-        self.limitTopic = count
+        self.topics.append(topic)
+        self.record?.setValue(topics, forKey: "topics")
     }
     
 }
