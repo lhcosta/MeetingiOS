94daffe48670b46766c008b7c7f7955811953690
Lucas <lucashpc91@gmail.com>
2019-12-03 00:10:07 -0300

    Tela New Meeting Iniciada

---
+++ MeetingiOS/MeetingiOS/Categories - ObjC/UIView+SetupBounds.h
@@ -0,0 +1,19 @@
+//
+//  UIView+SetupBounds.h
+//  MeetingiOS
+//
+//  Created by Lucas Costa  on 02/12/19.
+//  Copyright © 2019 Bernardo Nunes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (SetupBounds)
+
+- (void)setupBounds:(CACornerMask)maskCorners;
+
+@end
+
+NS_ASSUME_NONNULL_END
