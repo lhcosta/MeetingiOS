63c6f78c331b0b9e974ecfbc1248340b2bdd06c0
Bernardo Nunes <bernardonunes97@gmail.com>
2019-12-10 11:32:17 -0300

    mymeetings pronta (fetch, segmented control) faltando search bar

---
+++ MeetingiOS/MeetingiOS/CollectionViewCells/MyMeetingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyMeetingsTableViewCell.swift
+//  MeetingiOS
+//
+//  Created by Bernardo Nunes on 10/12/19.
+//  Copyright © 2019 Bernardo Nunes. All rights reserved.
+//
+
+import UIKit
+
+class MyMeetingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
