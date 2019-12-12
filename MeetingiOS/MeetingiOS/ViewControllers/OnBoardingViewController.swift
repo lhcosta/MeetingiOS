9eff8b2722f44b1f96514a2443ab014d6fac2d34
Bernardo Nunes <bernardonunes97@gmail.com>
2019-12-12 12:13:23 -0300

    Refresh mymeetings, ajustes search bar mymeetings finishedmeetings
    adicionando storyboards de tela de perfil e onboarding

---
+++ MeetingiOS/MeetingiOS/ViewControllers/OnBoardingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnBoardingViewController.swift
+//  MeetingiOS
+//
+//  Created by Bernardo Nunes on 12/12/19.
+//  Copyright © 2019 Bernardo Nunes. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
