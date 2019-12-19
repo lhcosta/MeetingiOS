fe2a102b977c2ef74e34cbbbcde8df3ea8848443
Caio Azevedo <caioba19@gmail.com>
2019-12-19 11:19:27 -0300

    Alteração da Tela das Conclusões adicionando campo de Descrição

--- MeetingiOS/MeetingiOS/ViewControllers/ConclusionsViewController.swift
+++ MeetingiOS/MeetingiOS/ViewControllers/ConclusionsViewController.swift
@@ -15,9 +15,11 @@ class ConclusionsViewController: UIViewController {
     @IBOutlet weak var viewTimer: UIView!
     
     
+    @IBOutlet weak var tableViewInfo: UITableView!
     @IBOutlet var authorNameLabel: UILabel!
     @IBOutlet var topicTimeLabel: UILabel!
-    @IBOutlet var conclusionsTableView: UITableView!
+    
+//    @IBOutlet var conclusionsTableView: UITableView!
     
     // Conclusions vindos do Tópico Seelecionado
     var topicToPresentConclusions: Topic!
@@ -32,21 +34,21 @@ class ConclusionsViewController: UIViewController {
         
         // Configruração da TableView
 //        conclusionsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        conclusionsTableView.delegate = self
-        conclusionsTableView.dataSource = self
+        tableViewInfo.delegate = self
+        tableViewInfo.dataSource = self
         
         // Arredondando bordas da View
-        viewTopic.layer.cornerRadius = 14
-        viewAuthor.layer.cornerRadius = 14
-        viewTimer.layer.cornerRadius = 14
+        viewTopic.layer.cornerRadius = 3
+        viewAuthor.layer.cornerRadius = 3
+        viewTimer.layer.cornerRadius = 3
         
         setShadow(view: viewTopic)
         setShadow(view: viewAuthor)
         setShadow(view: viewTimer)
         
-        setShadow(view: conclusionsTableView)
+        setShadow(view: tableViewInfo)
         
-        conclusionsTableView.layer.cornerRadius = 14
+        tableViewInfo.layer.cornerRadius = 3
         
         //Reconhece o gesto e o adiciona na tela
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
@@ -112,26 +114,41 @@ class ConclusionsViewController: UIViewController {
 extension ConclusionsViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return topicToPresentConclusions.conclusions.count
+        return 1
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "conCell", for: indexPath) as! ConclusionTableViewCell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cellDescription", for: indexPath) as! DescriptionTableViewCell
+            
+            setShadow(view: cell.descriptionView)
+            cell.descriptionLabel.text = " dsfgck"
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cellConclusion", for: indexPath) as! ConclusionInfoTableViewCell
+            cell.viewControler = self
+            cell.conclusionTableView.delegate = cell
+            cell.conclusionTableView.dataSource = cell
+            
+            return cell
+        }
         
-        cell.textConclusion.text = topicToPresentConclusions.conclusions[indexPath.row]
-        cell.textConclusion.delegate = self
         
-        return cell
+        
     }
     
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
+        if indexPath.section == 0 {
+            return 100
+        }
+        return tableView.frame.height
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -142,33 +159,8 @@ extension ConclusionsViewController: UITableViewDelegate, UITableViewDataSource 
             if (editingStyle == UITableViewCell.EditingStyle.delete) {
                 print("Index Pathhhh: \(indexPath.row)")
                 self.topicToPresentConclusions.conclusions.remove(at: indexPath.row)
-                self.conclusionsTableView.reloadData()
+//                self.conclusionsTableView.reloadData()
             }
         }
-    }
-}
-
-extension ConclusionsViewController: UITextFieldDelegate{
-    public func textFieldDidEndEditing(_ textField: UITextField) {
-        
-        // Obtem a referência da calular para a obtenção do indexPath referente
-        guard let cell = textField.superview?.superview as? ConclusionTableViewCell else { return }
-        let indexPath = conclusionsTableView.indexPath(for: cell)
-        
-        // Se o tópico estiva em branco então após a edição do mesmo adiciona um topico em branco abaixo para futura edição
-        if self.topicToPresentConclusions.conclusions[indexPath!.row] == ""{
-            self.topicToPresentConclusions.conclusions[indexPath!.row] = textField.text!
-            self.topicToPresentConclusions.conclusions.append(String())
-        } else {
-            self.topicToPresentConclusions.conclusions[indexPath!.row] = textField.text!
-        }
-        
-        // Atualiza os dados da tableView
-        self.conclusionsTableView.reloadData()
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
     }
 }
