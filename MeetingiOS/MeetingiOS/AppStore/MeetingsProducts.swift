ad7b51928622dd02ce7db87cc035665cf7860841
Bernardo Nunes <bernardonunes97@gmail.com>
2020-01-29 15:01:08 -0300

    assinatura premium (parcialmente pronto)

---
+++ MeetingiOS/MeetingiOS/AppStore/MeetingsProducts.swift
@@ -0,0 +1,15 @@
+//
+//  MeetingsProducts.swift
+//  MeetingiOS
+//
+//  Created by Bernardo Nunes on 28/01/20.
+//  Copyright © 2020 Bernardo Nunes. All rights reserved.
+//
+
+import Foundation
+
+struct  MeetingsProducts {
+    let month = "id"
+    let threeMonths = "id2"
+    let year = "id3"
+}
--- MeetingiOS/MeetingiOS/ViewControllers/MyMeetingsViewController.swift
+++ MeetingiOS/MeetingiOS/ViewControllers/MyMeetingsViewController.swift
@@ -34,7 +34,7 @@ import CloudKit
     //MARK:- View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
         meetings.append([Meeting]())
         meetings.append([Meeting]())
         meetingsToShow = meetings[0]
@@ -46,7 +46,8 @@ import CloudKit
         // MARK: Nav Controller Settings
         self.navigationItem.title = NSLocalizedString("My meetings", comment: "")
         self.navigationItem.hidesBackButton = true
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "perfil", style: .plain, target: self, action: #selector(goToProfile))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: self, action: #selector(goToProfile))
+       
         self.setUpSearchBar(segmentedControlTitles: [NSLocalizedString("Future meetings", comment: ""), NSLocalizedString("Past meetings", comment: "")])
         
         // MARK: Query no CK
