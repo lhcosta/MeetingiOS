75c3b096f5296aa0f32ec39320ba4cb2b26b98a6
Lucas <lucashpc91@gmail.com>
2020-02-07 11:25:02 -0300

    Apresentacao TVs alterada.

---
+++ MeetingiOS/MeetingiOS/ViewControllers/SelectionTVsViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SelectionTVsViewController.swift
+//  MeetingiOS
+//
+//  Created by Lucas Costa  on 07/02/20.
+//  Copyright © 2020 Bernardo Nunes. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class SelectionTVsViewController: UIViewController {
+    
+    //MARK:- Properties
+    private var tvsTableView : TvsTableView!
+    private var tvsTableViewData : TvsTableViewData!
+    var meeting : Meeting!
+    private var multipeer : MeetingBrowserPeer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(selectedTv(notification:)), name: Notification.Name(rawValue:"SelectedTV"), object: nil)
+        
+        self.tvsTableView = TvsTableView()
+        self.tvsTableViewData = TvsTableViewData(tvsTableView)
+        
+        view.addSubview(tvsTableView)
+        
+        NSLayoutConstraint.activate([
+            tvsTableView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8),
+            tvsTableView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3),
+            tvsTableView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tvsTableView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        tvsTableView.awakeFromNib()
+        tvsTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.multipeer = MeetingBrowserPeer(tvsTableViewData)
+    }
+    
+    /// Recebendo o peer selecionado através de uma notificação enviada.
+    /// - Parameter notification: notificacao enviada.
+    @objc private func selectedTv(notification : NSNotification) {
+        
+        guard let peerId = notification.object as? MCPeerID else {return}
+        
+        let encoder = JSONEncoder()
+        if let data = try? encoder.encode(self.meeting) {
+            self.multipeer?.sendInviteFromPeer(peerID: peerId, dataToSend: data, completionHandler: {
+                self.dismiss(animated: true, completion: nil)
+            })
+        }
+    }
+    
+    
+}
