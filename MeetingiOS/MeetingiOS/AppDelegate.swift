0fa2003c2aa78047d622642e1fd6aed49dde91f4
Lucas <lucashpc91@gmail.com>
2020-02-09 12:58:55 -0300

    Removendo metodo do AppDelegate

--- MeetingiOS/MeetingiOS/AppDelegate.swift
+++ MeetingiOS/MeetingiOS/AppDelegate.swift
@@ -16,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        StoreManager.shared.restoreSubscription()
+        
+        //Removendo metodo subscription
+        //StoreManager.shared.restoreSubscription()
+        
         return true
     }
     
